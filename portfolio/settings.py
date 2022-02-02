54b259926bdb47ab4d9997285e28bf62b39e76e1
andregrandon <gran1360@mylaurier.ca>
2022-02-01 19:31:49 -0500

    add static file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -103,6 +103,43 @@ AUTH_PASSWORD_VALIDATORS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+# Debugging in heroku live
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'formatters': {
+       'verbose': {
+           'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                      'pathname=%(pathname)s lineno=%(lineno)s ' +
+                      'funcname=%(funcName)s %(message)s'),
+           'datefmt': '%Y-%m-%d %H:%M:%S'
+       },
+       'simple': {
+           'format': '%(levelname)s %(message)s'
+       }
+   },
+   'handlers': {
+       'null': {
+           'level': 'DEBUG',
+           'class': 'logging.NullHandler',
+       },
+       'console': {
+           'level': 'DEBUG',
+           'class': 'logging.StreamHandler',
+           'formatter': 'verbose'
+       }
+   },
+   'loggers': {
+       'testlogger': {
+           'handlers': ['console'],
+           'level': 'INFO',
+       }
+   }
+}
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
