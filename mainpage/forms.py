d81c2be165c8b0a524afc590a3c76387bd85f4fb
andregrandon <gran1360@mylaurier.ca>
2022-02-04 17:55:55 -0500

    push

--- mainpage/forms.py
+++ mainpage/forms.py
@@ -9,8 +9,8 @@ class ContactForm(forms.Form):
    attrs={'placeholder': 'Name', 'class': 'form-control', 'id':'c_name'}))
 
    email = forms.EmailField(label='sr-only', widget=forms.TextInput(
-   attrs={'placeholder': 'Subject', 'class': 'form-control', 'id':'c_email'}),
-   required=False)
+    attrs={'placeholder': 'E-mail', 'class': 'form-control', 'id':'c_email'}),
+    required=False)
 
    message = forms.CharField(label='sr-only', widget=forms.TextInput(
    attrs={'placeholder': 'Message (Include contact info here as well!)', 'class': 'form-control', 'id':'c_message'}))
