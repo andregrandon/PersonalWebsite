26bb46e410b139b32e319907acc8a3502b432c93
andregrandon <gran1360@mylaurier.ca>
2022-02-04 19:01:55 -0500

    subject

--- mainpage/forms.py
+++ mainpage/forms.py
@@ -8,9 +8,9 @@ class ContactForm(forms.Form):
    name = forms.CharField(label='sr-only', widget=forms.TextInput(
    attrs={'placeholder': 'Name', 'class': 'form-control', 'id':'c_name'}))
 
-   email = forms.EmailField(label='sr-only', widget=forms.TextInput(
-    attrs={'placeholder': 'E-mail', 'class': 'form-control', 'id':'c_email'}),
-    required=False)
+   email = forms.CharField(label='sr-only', widget=forms.TextInput(
+     attrs={'placeholder': 'Subject', 'class': 'form-control', 'id':'c_email'}),
+     required=False)
 
    message = forms.CharField(label='sr-only', widget=forms.TextInput(
-   attrs={'placeholder': 'Message (Include contact info here as well!)', 'class': 'form-control', 'id':'c_message'}))
+   attrs={'placeholder': 'Message/Email', 'class': 'form-control', 'id':'c_message'}))
