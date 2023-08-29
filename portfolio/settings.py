c0d79d709a65f14ee7a06cc00adc7d4752eedb92
Andre Grandon <andre@andresmacstudio.home>
2023-08-29 15:03:46 -0400

    change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DEBUG', True) )
 
-ALLOWED_HOSTS =  ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
